ff595e9aad4a3c1f3316eceac5c2f513f1f48a7f
IShungite <ishungite@gmail.com>
2022-03-15 10:13:52 +0100

    Cant go to the login page if already login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from "next-auth/react";
+import { ClientSafeProvider, getProviders, getSession, LiteralUnion, signIn } from "next-auth/react";
 import { BuiltInProviderType } from "next-auth/providers";
 import { Box, Button, Typography } from "@mui/material";
+import { RouteUrls } from "../../utility/config";
+import { GetServerSideProps } from "next";
 
 export default function Login({ providers }: { providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> }) {
   return (
@@ -19,7 +21,18 @@ export default function Login({ providers }: { providers: Record<LiteralUnion<Bu
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  // If the session already exists, redirect to home
+  if (session) {
+    context.res.writeHead(302, { Location: RouteUrls.Home });
+    context.res.end();
+
+    return { props: {} };
+  }
+
+  // Get the providers that will be sent to the Login page (spotify provider, ..)
   const providers = await getProviders();
 
   return {
