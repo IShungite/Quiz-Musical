5eff70c3f25610db3a2c793850913656805bebc3
IShungite <IShungite@gmail.com>
2022-04-14 18:10:28 +0200

    update api url

--- src/utility/deezerApi.ts
+++ src/utility/deezerApi.ts
@@ -4,7 +4,7 @@ import { Track } from "../models/Tracks";
 import { tryFetch } from "./utility";
 
 // const baseUrl = "https://cors-anywhere.herokuapp.com/http://api.deezer.com/";
-const baseUrl = "http://api.deezer.com/";
+const baseUrl = "https://api.deezer.com/";
 
 const searchPlaylists = async (searchTerm: string): Promise<Playlist[]> => {
   return tryFetch<Playlist[]>(`${baseUrl}search/playlist?limit=15&q=${searchTerm}`);
