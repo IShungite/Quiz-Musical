4d2796f5d19a4e7302a5d3806972c93344d3b286
IShungite <IShungite@gmail.com>
2022-04-14 18:02:42 +0200

    update

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
       },
       {
         // matching all API routes
-        source: "http://api.deezer.com/search/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
